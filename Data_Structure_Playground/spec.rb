73701c421c76432fc7cdb0ceec27f8badbf7bab6
Reuben Brandt <Reubennbrandt@gmail.com>
2016-08-25 21:59:58 -0700

    Add early test structure

--- Data_Structure_Playground/spec.rb
+++ Data_Structure_Playground/spec.rb
@@ -6,4 +6,12 @@ require_relative 'node_sorter'
 require_relative 'Binary_search_tree'
 require 'rspec'
 
+describe '#Linked_list' do
 
+  context "when a new linked list object is created" do
+    let(:list){Linked_list.new}
+      it "has a tail with a nil value" do
+      end
+    end
+  end
+end
