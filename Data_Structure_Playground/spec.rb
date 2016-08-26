e8b7385598c2aebd087e2be418a472d08f59e2ac
Reuben Brandt <Reubennbrandt@gmail.com>
2016-08-25 20:20:48 -0700

    create initial spec file

---
+++ Data_Structure_Playground/spec.rb
@@ -0,0 +1,9 @@
+require_relative 'min_helper'
+require_relative 'linked_lists'
+require_relative 'max_heap'
+require_relative 'min_heap'
+require_relative 'node_sorter'
+require_relative 'Binary_search_tree'
+require 'rspec'
+
+
