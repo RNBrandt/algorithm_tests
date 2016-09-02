17d4ae44b9b58e715860d873965a8b10a1bfbb28
Reuben Brandt <Reubennbrandt@gmail.com>
2016-09-02 14:41:30 -0700

    Add Gemfile

--- Data_Structure_Playground/spec.rb
+++ Data_Structure_Playground/spec.rb
@@ -1,17 +1,9 @@
 require_relative 'min_helper'
-require_relative 'linked_lists'
+
 require_relative 'max_heap'
 require_relative 'min_heap'
 require_relative 'node_sorter'
 require_relative 'Binary_search_tree'
-require 'rspec'
 
-describe '#Linked_list' do
 
-  context "when a new linked list object is created" do
-    let(:list){Linked_list.new}
-      it "has a tail with a nil value" do
-      end
-    end
-  end
-end
+
---
+++ Data_Structure_Playground/spec/linked_list_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../linked_lists'
+
+describe '#Linked_list' do
+
+  context "when a new linked list object is created" do
+    let(:list){Linked_list.new}
+      it "has a tail with a nil value" do
+      end
+    end
+  end
+end
